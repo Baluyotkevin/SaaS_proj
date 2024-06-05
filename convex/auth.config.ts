3b1e90ea297ed8081fcfb621fabb6acc3b49f79f
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-06-05 12:31:34 -0700

    setting up middleware/config/webhooks with Clerk and Convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://stirring-killdeer-22.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
